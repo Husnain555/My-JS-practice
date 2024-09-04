ef5ef9997d01da0847ad8a55eb53adee03a2d2e7
Husnain <talk2husnain@gmail.com>
2024-09-05 00:37:05 +0500

    improvements

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -1,10 +1,24 @@
-function experiments(){
-    let count = 0
-    document.getElementById('CLICK').addEventListener('click', ()=>{
-        console.log('husnain', ++count);
-    })
-}
+// ..........................Add event Lisner with function clouser method...............
 
-let abc;
-abc = experiments();
-console.log(abc)
+// function experiments() {
+//     let count = 0
+//     document.getElementById('CLICK').addEventListener('click', function abc() {
+//         window.console.log('Husnain', ++count)
+//
+//     })
+// }
+// experiments();
+// ...............blocking the main threed...........................
+console.log('husnain')
+
+setTimeout(()=>{
+    console.log('babar')
+},5000)
+
+let newDate = new Date().getTime()
+let oldDate = newDate
+while (oldDate < newDate + 10000) {
+    oldDate = new Date().getTime()
+}
+console.log(`zman`)
+console.log('awais')
